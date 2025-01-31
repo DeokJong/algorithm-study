abdf56d3c25098c5183a55e1c522c3b4744c8eb1
DeokJong <129056857+DeokJong@users.noreply.github.com>
2025-01-31 14:33:30 +0900

    [Silver III] Title: 바이러스, Time: 68 ms, Memory: 11592 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,76 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Queue;
-import java.util.Scanner;
 
 public class Main {
-	private static Scanner sc = new Scanner(System.in);
-	
-	public static class Edge {
-		int start;
-		int end;
-		public Edge(int start, int end) {
-			super();
-			this.start = start;
-			this.end = end;
-		}
-		public int getStart() {
-			return start;
-		}
-		public void setStart(int start) {
-			this.start = start;
-		}
-		public int getEnd() {
-			return end;
-		}
-		public void setEnd(int end) {
-			this.end = end;
-		}
-	}
-	
+	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	private static int pcCount;
 	private static int edgeCount;
-	private static Edge[] edges;
+	private static List<List<Integer>> graph;
+	private static Queue<Integer> pcQueue = new ArrayDeque<>();
+	private static int virusCount = -1;
 	private static boolean[] visited;
-	private static Queue<Integer> queue = new ArrayDeque<>();
-	private static int viruscount = -1;
 	
-	public static void main(String[] args) {
-		initializeProblem();
-		solve();
-		System.out.println(viruscount != -1 ? viruscount : 0);
+	public static void main(String[] args) throws IOException {
+    initializeProblem();
+    sovle();
+    System.out.println(virusCount != -1 ? virusCount : 0);
 	}
 	
-	private static void initializeProblem() {
-		pcCount = sc.nextInt();
-		sc.nextLine();
+	private static void initializeProblem() throws IOException {
+		pcCount = Integer.parseInt(br.readLine());
+		edgeCount = Integer.parseInt(br.readLine());
+		
 		visited = new boolean[pcCount];
+		graph = new ArrayList<>();
+		for (int i = 0; i < pcCount; i++) {
+		    graph.add(new ArrayList<>());
+		}
 		
-		edgeCount = sc.nextInt();
-		sc.nextLine();
-		edges = new Edge[edgeCount];
-		
-		for(int i =0;i<edgeCount;i++) {
-			edges[i] = new Edge(sc.nextInt()-1, sc.nextInt()-1);
-			sc.nextLine();
+		for(int i =0;i<edgeCount; i++) {
+      String[] input = br.readLine().split(" ");
+      int p1 = Integer.parseInt(input[0])-1;
+      int p2 = Integer.parseInt(input[1])-1;
+      
+      graph.get(p1).add(p2);
+      graph.get(p2).add(p1);
 		}
 	}
 	
-	private static void solve() {
-		queue.add(0);
+	private static void sovle() {
+		pcQueue.add(0);
 		
-		while (!queue.isEmpty()){
-			int pc = queue.poll();
+		while (!pcQueue.isEmpty()) {
+			int pc = pcQueue.poll();
 			
-			if(visited[pc]) {
-				continue ;
+			if (visited[pc]) {
+				continue;
 			} else {
 				visited[pc] = true;
-				viruscount++;
-				for(Edge edge : edges) {
-					if (edge.getStart() == pc && !visited[edge.getEnd()]) {
-						queue.add(edge.getEnd());
-					} else if (edge.getEnd() == pc && !visited[edge.getStart()]) {
-						queue.add(edge.getStart());
+				virusCount++;
+				
+				for(int neighbor : graph.get(pc)) {
+					if(!visited[neighbor]) {
+						pcQueue.add(neighbor);
 					}
 				}
 			}
