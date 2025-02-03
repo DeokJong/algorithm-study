46dac70bd8224f3b2a963cf94065a76e05968d54
DeokJong <129056857+DeokJong@users.noreply.github.com>
2025-02-04 00:00:28 +0900

    [Silver I] Title: 포도주 시식, Time: 192 ms, Memory: 40960 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -1,38 +1,55 @@
 import java.util.Scanner;
 
 public class Main {
-    private static Scanner sc = new Scanner(System.in);
-    private static int[] wines;
-    private static int[] dp;
-    private static int wineCount;
+  private static Scanner sc = new Scanner(System.in);
+  private static int[] wines;
+  private static int wineCount;
 
-    public static void main(String[] args) {
-        initializeProblem();
-        solve();
-        System.out.println(dp[wineCount]);
+  private static int prev3;
+  private static int prev2;
+  private static int prev1;
+  private static int current;
+
+  public static void main(String[] args) {
+    initializeProblem();
+    solve();
+    System.out.println(current);
+  }
+
+  private static void initializeProblem() {
+    wineCount = Integer.parseInt(sc.nextLine());
+    wines = new int[wineCount + 1];
+
+    for (int i = 1; i <= wineCount; i++) {
+      wines[i] = Integer.parseInt(sc.nextLine());
+    }
+  }
+
+  private static void solve() {
+    if (wineCount == 0)
+      return;
+
+    prev3 = 0;
+    prev2 = 0;
+    prev1 = 0;
+    current = wines[1];
+
+    if (wineCount >= 2) {
+      prev1 = current;
+      current = wines[1] + wines[2];
     }
 
-    private static void initializeProblem() {
-        wineCount = Integer.parseInt(sc.nextLine());
-        wines = new int[wineCount + 1];
-        dp = new int[wineCount + 1];
-
-        for (int i = 1; i <= wineCount; i++) {
-            wines[i] = Integer.parseInt(sc.nextLine());
-        }
+    if (wineCount >= 3) {
+      prev2 = prev1;
+      prev1 = current;
+      current = Math.max(wines[1]+ wines[3],  Math.max(wines[2]+ wines[3], wines[1]+wines[2])) ;
     }
 
-    private static void solve() {
-        if (wineCount == 0) return;
-
-        dp[1] = wines[1];
-
-        if (wineCount >= 2) {
-            dp[2] = wines[1] + wines[2];
-        }
-
-        for (int i = 3; i <= wineCount; i++) {
-            dp[i] = Math.max(dp[i - 1], Math.max(dp[i - 2] + wines[i], dp[i - 3] + wines[i - 1] + wines[i]));
-        }
+    for (int i = 4; i <= wineCount; i++) {
+      prev3 = prev2;
+      prev2 = prev1;
+      prev1 = current;
+      current = Math.max(prev1, Math.max(prev2 + wines[i], prev3 + wines[i - 1] + wines[i]));
     }
+  }
 }
