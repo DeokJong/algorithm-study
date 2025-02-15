3874d8d7528c0267ad599e03f881c0944b919ab6
DeokJong <129056857+DeokJong@users.noreply.github.com>
2025-02-15 16:24:44 +0900

    [Silver III] Title: 괄호 끼워넣기, Time: 68 ms, Memory: 11688 KB -BaekjoonHub

---
+++ 백준/Silver/11899. 괄호 끼워넣기/괄호 끼워넣기.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main{
+	private static Stack<String> stack = new Stack<>();
+	
+	public static void main(String[] args) throws IOException {
+		solve();
+	}
+	
+	private static void solve() throws IOException {
+		String[] inputs = (new BufferedReader(new InputStreamReader(System.in))).readLine().split("");
+		for(String input : inputs) {
+			if(stack.isEmpty()) {
+				stack.add(input);
+				continue ;
+			}
+			
+			if(stack.peek().equals(input)) {
+				stack.add(input);
+				continue;
+			}
+			
+			if(input.equals(")")) {
+				stack.pop();
+			} else {
+				stack.add(input);
+			}
+			
+			
+		}
+		System.out.println(stack.size());
+	}
+}
