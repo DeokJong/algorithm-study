a4ec052495001d28dc7a1ef9bc0224f5bdfc4faa
DeokJong <129056857+DeokJong@users.noreply.github.com>
2025-02-15 22:22:52 +0900

    [Gold IV] Title: 문자열 폭발, Time: 280 ms, Memory: 117968 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -1,10 +1,12 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     public static void main(String[] args) throws IOException {
         solve();
     }
@@ -25,9 +27,13 @@ public class Main {
         }
 
         if (sb.length() == 0) {
-            System.out.println("FRULA");
+        	bw.write("FRULA");
         } else {
-            System.out.println(sb.toString());
+        	bw.write(sb.toString());
         }
+        
+        bw.flush();
+        br.close();
+        bw.close();
     }
 }
