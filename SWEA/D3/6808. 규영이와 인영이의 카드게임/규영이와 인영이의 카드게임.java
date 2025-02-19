5a09030a85f4019ae62ffb54774d46ec11137519
DeokJong <129056857+DeokJong@users.noreply.github.com>
2025-02-19 14:18:09 +0900

    [D3] Title: 규영이와 인영이의 카드게임, Time: 1,445 ms, Memory: 101,320 KB -BaekjoonHub

--- SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
+++ SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
@@ -1,81 +1,102 @@
 import static java.lang.Integer.parseInt;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Solution {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static int leftCtn;
-    static int rightCtn;
-    static int[][] cards = new int[2][];
-    static int leftRemainSum;
-    static int rightRemainSum;
-
+    
+    // 왼쪽 카드와 오른쪽 카드 배열
+    static int[] leftCards;
+    static int[] rightCards;
+    
+    // dp 메모이제이션: key = "mask,diff", value = {왼쪽 승리 횟수, 오른쪽 승리 횟수}
+    static HashMap<String, int[]> dp;
+    
     public static void main(String[] args) throws IOException {
         int T = parseInt(br.readLine());
         for (int t = 1; t <= T; t++) {
-            leftCtn = 0;
-            rightCtn = 0;
             initializeProblem();
-            solve(0, 0, 0, 0);
-            System.out.println("#" + t + " " + leftCtn + " " + rightCtn);
+            dp = new HashMap<>();
+            // 초기 상태: mask=0, depth=0, diff=0
+            int[] result = solve(0, 0, 0);
+            System.out.println("#" + t + " " + result[0] + " " + result[1]);
         }
     }
-
+    
+    // 문제 초기화: 왼쪽 카드 입력 및 오른쪽 카드 생성(1~18 중 사용되지 않은 카드)
     private static void initializeProblem() throws IOException {
-        int[] left = new int[9];
+        leftCards = new int[9];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int j = 0; j < 9; j++) {
-            left[j] = parseInt(st.nextToken());
+            leftCards[j] = parseInt(st.nextToken());
         }
-        cards[0] = left.clone();
-        leftRemainSum = Arrays.stream(left).sum();
-        boolean[] used = new boolean[19];
-        for (int card : left) {
+        boolean[] used = new boolean[19];  // 1부터 18까지
+        for (int card : leftCards) {
             used[card] = true;
         }
-        int[] right = new int[9];
+        rightCards = new int[9];
         int idx = 0;
         for (int num = 1; num <= 18; num++) {
             if (!used[num]) {
-                right[idx++] = num;
+                rightCards[idx++] = num;
             }
         }
-        cards[1] = right;
-        rightRemainSum = Arrays.stream(right).sum();
     }
-
-    private static void solve(int masks, int depth, int leftSum, int rightSum) {
+    
+    /**
+     * dp(메모이제이션)을 적용한 재귀 함수
+     * 
+     * @param mask  오른쪽 카드 사용 여부를 비트마스크로 표현 (9자리)
+     * @param depth 현재까지 진행한 라운드 (0~9; 사실 mask에 설정된 비트 개수와 동일)
+     * @param diff  현재까지의 점수 차 (leftSum - rightSum)
+     * @return int[] 배열 {왼쪽 승리 경우의 수, 오른쪽 승리 경우의 수}
+     */
+    private static int[] solve(int mask, int depth, int diff) {
+        // 모든 라운드가 끝난 경우: 승리 여부에 따라 경우의 수를 반환
         if (depth == 9) {
-            if (leftSum > rightSum)
-                leftCtn++;
-            else if (leftSum < rightSum)
-                rightCtn++;
-            return;
-        }
-        int leftCard = cards[0][depth];
-        for (int r = 0; r < 9; r++) {
-            int rightBit = 1 << r;
-            if ((masks & rightBit) != 0)
-                continue;
-            masks |= rightBit;
-            int rightCard = cards[1][r];
-            int score = leftCard + rightCard;
-            if (leftCard > rightCard)
-                solve(masks, depth + 1, leftSum + score, rightSum);
+            if (diff > 0)
+                return new int[]{1, 0};
+            else if (diff < 0)
+                return new int[]{0, 1};
             else
-                solve(masks, depth + 1, leftSum, rightSum + score);
-            masks &= ~rightBit;
+                return new int[]{0, 0};  // 문제에 따라 무승부 처리 (여기서는 0으로 처리)
         }
-    }
-
-    static int factorial(int p) {
-        int rtn = 1;
-        for (int i = 2; i <= p; i++) {
-            rtn *= i;
+        
+        // dp 메모이제이션 키 구성 (diff가 음수일 수 있으므로 문자열로 구성)
+        String key = mask + "," + diff;
+        if (dp.containsKey(key)) {
+            return dp.get(key);
         }
-        return rtn;
+        
+        int leftWins = 0;
+        int rightWins = 0;
+        int leftCard = leftCards[depth];
+        
+        // 오른쪽 카드 중 아직 사용하지 않은 카드에 대해 모든 경우의 수 탐색
+        for (int r = 0; r < 9; r++) {
+            int bit = 1 << r;
+            if ((mask & bit) != 0)
+                continue;
+            int newMask = mask | bit;
+            int rightCard = rightCards[r];
+            int score = leftCard + rightCard;
+            int newDiff = diff;
+            if (leftCard > rightCard)
+                newDiff += score;
+            else
+                newDiff -= score;
+            
+            int[] subResult = solve(newMask, depth + 1, newDiff);
+            leftWins += subResult[0];
+            rightWins += subResult[1];
+        }
+        
+        int[] result = new int[]{leftWins, rightWins};
+        dp.put(key, result);
+        return result;
     }
 }
